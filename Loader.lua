e2bb73ca5adbca19e4d91c4484f8d7a07dfb3589
lynxloader <162089130+lynxloader@users.noreply.github.com>
2024-05-04 20:37:48 +0630

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,7 @@
+if game.PlaceId == 2753915549 or game.PlaceId == 4442272183 or game.PlaceId == 7449423635 then
+  pcall(function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/Script-CSVN/HiruHub/main/Script.lua))()
+  end)
+else
+    game.Players.LocalPlayer:Destroy()
+end
